3ff5dc578a161b206910fe922a9df71de85803df
fearless-soul-1 <shikharkatiyar558@gmail.com>
2025-06-24 23:34:41 +0530

    Integrating Redux with react

---
+++ task-13/trainerChallenge/redux-demo.js
@@ -0,0 +1,42 @@
+const redux = require('redux');
+
+const counterReducer = (state = { counter : 0 }, action) =>{
+    if (action.type === 'increment'){
+        return{
+        counter: state.counter + 1
+    };
+    }
+
+    if (action.type === 'decrement'){
+        return{
+            counter : state.counter - 1
+        }
+    }
+
+    if (action.type === 'INCREMENTBY2') {
+        return { counter: state.counter + 2 };
+    }
+
+    if (action.type === 'DECREMENTBY2') {
+        return { counter: state.counter - 2 };
+    }
+
+    return state;
+};
+
+const store = redux.createStore(counterReducer);
+
+// console.log(store.getState());
+
+const counterSubscriber = () =>{
+    const latestState = store.getState();
+    console.log(latestState);
+}
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: 'increment' });
+store.dispatch({ type: 'decrement' });
+
+store.dispatch({ type: 'INCREMENTBY2' }); // Increases by 2
+store.dispatch({ type: 'DECREMENTBY2' }); // Decreases by 2
